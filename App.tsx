231ae5ff5864ab128459805db5afaff3a04fe0df
phamha98 <phamtranquangha@gmail.com>
2025-10-30 09:30:18 +0700

    ✨ feat(drawer): implement drawer layout component
    - add basic drawer layout with configurable content and menu
    - enable dynamic sizing and positioning of drawer

--- App.tsx
+++ App.tsx
@@ -1,9 +1,4 @@
 //@ts-nocheck
-//@ts-nocheck
-//@ts-nocheck
-//@ts-nocheck
-//@ts-nocheck
-//@ts-nocheck
 import React from 'react'
 import { Dimensions, Text, View } from 'react-native'
 import DrawerLayout from './index'
